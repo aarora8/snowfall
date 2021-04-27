e1ab8088483099aba0e498641a0b03a72a042238
aarora8 <aarora8@jhu.edu>
2021-04-27 17:07:56 -0400

    adding data prep scripts

---
+++ egs/safet/asr/simple_v1/local2/safet_train_lms.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# To be run from one directory above this script.
+. ./path.sh
+
+text=data/local/train/text
+lexicon=data/local/dict_nosp/lexicon.txt
+
+for f in "$text" "$lexicon"; do
+  [ ! -f $x ] && echo "$0: No such file $f" && exit 1
+done
+
+# This script takes no arguments.  It assumes you have already run
+# aishell_data_prep.sh.
+# It takes as input the files
+# data/local/train/text
+# data/local/dict_nosp/lexicon.txt
+dir=data/local/lm
+mkdir -p $dir
+
+kaldi_lm=$(which train_lm.sh)
+if [ -z $kaldi_lm ]; then
+  echo "$0: train_lm.sh is not found. That might mean it's not installed"
+  echo "$0: or it is not added to PATH"
+  echo "$0: Please use the following commands to install it"
+  echo "  git clone https://github.com/danpovey/kaldi_lm.git"
+  echo "  cd kaldi_lm"
+  echo "  make -j"
+  echo "Then add the path of kaldi_lm to PATH and rerun $0"
+  exit 1
+fi
+
+cleantext=$dir/text.no_oov
+exit
+cat $text | awk -v lex=$lexicon 'BEGIN{while((getline<lex) >0){ seen[$1]=1; } }
+  {for(n=1; n<=NF;n++) {  if (seen[$n]) { printf("%s ", $n); } else {printf("<UNK> ");} } printf("\n");}' \
+  >$cleantext || exit 1
+
+cat $cleantext | awk '{for(n=2;n<=NF;n++) print $n; }' | sort | uniq -c |
+  sort -nr >$dir/word.counts || exit 1
+
+# Get counts from acoustic training transcripts, and add  one-count
+# for each word in the lexicon (but not silence, we don't want it
+# in the LM-- we'll add it optionally later).
+cat $cleantext | awk '{for(n=2;n<=NF;n++) print $n; }' |
+  cat - <(grep -w -v '!SIL' $lexicon | awk '{print $1}') |
+  sort | uniq -c | sort -nr >$dir/unigram.counts || exit 1
+
+# note: we probably won't really make use of <UNK> as there aren't any OOVs
+cat $dir/unigram.counts | awk '{print $2}' | get_word_map.pl "<s>" "</s>" "<UNK>" >$dir/word_map ||
+  exit 1
+
+# note: ignore 1st field of train.txt, it's the utterance-id.
+cat $cleantext | awk -v wmap=$dir/word_map 'BEGIN{while((getline<wmap)>0)map[$1]=$2;}
+  { for(n=2;n<=NF;n++) { printf map[$n]; if(n<NF){ printf " "; } else { print ""; }}}' | gzip -c >$dir/train.gz ||
+  exit 1
+
+train_lm.sh --arpa --lmtype 3gram-mincount $dir || exit 1
+exit 0
--- egs/safet/asr/simple_v1/run.sh
+++ egs/safet/asr/simple_v1/run.sh
@@ -8,73 +8,51 @@
 set -eou pipefail
 [ -f path.sh ] && . ./path.sh
 
-stage=5
+stage=2
+if [ $stage -le 0 ]; then
+  local2/prepare_dict.sh data/local/lm data/local/dict_nosp
+fi
 
 if [ $stage -le 1 ]; then
-  local/download_lm.sh "openslr.org/resources/11" data/local/lm
-fi
-
-if [ $stage -le 2 ]; then
-  local/prepare_dict.sh data/local/lm data/local/dict_nosp
-fi
-
-if [ $stage -le 3 ]; then
   local/prepare_lang.sh \
     --position-dependent-phones false \
     data/local/dict_nosp \
     "<UNK>" \
     data/local/lang_tmp_nosp \
     data/lang_nosp
-
-  echo "To load L:"
-  echo "    Lfst = k2.Fsa.from_openfst(<string of data/lang_nosp/L.fst.txt>, acceptor=False)"
 fi
 
 if [ $stage -le 4 ]; then
-  # Build G
-  python3 -m kaldilm \
-    --read-symbol-table="data/lang_nosp/words.txt" \
-    --disambig-symbol='#0' \
-    --max-order=1 \
-    data/local/lm/lm_tgmed.arpa >data/lang_nosp/G_uni.fst.txt
+  echo "LM preparation"
+  local2/aishell_train_lms.sh
+  gunzip -c data/local/lm/3gram-mincount/lm_unpruned.gz >data/local/lm/lm_tgmed.arpa
 
+  # Build G
   python3 -m kaldilm \
     --read-symbol-table="data/lang_nosp/words.txt" \
     --disambig-symbol='#0' \
     --max-order=3 \
     data/local/lm/lm_tgmed.arpa >data/lang_nosp/G.fst.txt
+fi
 
+if [ $stage -le 2 ]; then
+  # Build G
   python3 -m kaldilm \
     --read-symbol-table="data/lang_nosp/words.txt" \
     --disambig-symbol='#0' \
-    --max-order=4 \
-    data/local/lm/lm_fglarge.arpa >data/lang_nosp/G_4_gram.fst.txt
-
-  echo ""
-  echo "To load G:"
-  echo "Use::"
-  echo "  with open('data/lang_nosp/G.fst.txt') as f:"
-  echo "    G = k2.Fsa.from_openfst(f.read(), acceptor=False)"
-  echo ""
+    --max-order=3 \
+    data/local2/lm/lm_tgmed.arpa >data/lang_nosp/G.fst.txt
 fi
-
-if [ $stage -le 5 ]; then
+exit
+if [ $stage -le 3 ]; then
   python3 ./prepare.py
 fi
 exit
-if [ $stage -le 6 ]; then
-  # python3 ./train.py # ctc training
-  # python3 ./mmi_bigram_train.py # ctc training + bigram phone LM
-  #  python3 ./mmi_mbr_train.py
-
-  # Single node, multi-GPU training
-  # Adapting to a multi-node scenario should be straightforward.
+if [ $stage -le 4 ]; then
   ngpus=1
   python3 -m torch.distributed.launch --nproc_per_node=$ngpus ./mmi_bigram_train.py --world_size $ngpus
 fi
 
-if [ $stage -le 7 ]; then
-  # python3 ./decode.py # ctc decoding
+if [ $stage -le 5 ]; then
   python3 ./mmi_bigram_decode.py --epoch 9
-  #  python3 ./mmi_mbr_decode.py
 fi
