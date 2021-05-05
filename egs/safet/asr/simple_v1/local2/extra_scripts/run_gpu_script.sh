1f05db9942d50a034a7e6c0048419769eb86f6e6
aarora8 <aarora8@jhu.edu>
2021-05-05 12:21:33 -0400

    run gpu scrupt

--- egs/safet/asr/simple_v1/local2/extra_scripts/run_gpu_script.sh
+++ egs/safet/asr/simple_v1/local2/extra_scripts/run_gpu_script.sh
@@ -4,3 +4,7 @@ source /home/gqin2/scripts/acquire-gpus 1
 source $HOME/miniconda3/bin/activate
 conda activate k2
 /home/aaror/miniconda3/envs/k2/bin/python3 /export/c03/aarora8/snowfall/egs/safet/asr/simple_v1/mmi_bigram_train.py
+
+#. ./cmd.sh
+#utils/queue.pl --mem 10G --gpu 1 train.log /home/aaror/miniconda3/envs/k2/bin/python mmi_bigram_train.py
+
