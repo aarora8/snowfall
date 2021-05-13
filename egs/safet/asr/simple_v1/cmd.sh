1713bba7909b3637bb51cd5f158db8f7b5dbc4f6
aarora8 <ashish.arora.88888@gmail.com>
2021-05-13 15:38:00 -0400

    adding changes to run snowfall on coe

--- egs/safet/asr/simple_v1/cmd.sh
+++ egs/safet/asr/simple_v1/cmd.sh
@@ -10,11 +10,18 @@
 # conf/queue.conf in http://kaldi-asr.org/doc/queue.html for more information,
 # or search for the string 'default_config' in utils/queue.pl or utils/slurm.pl.
 
-#export train_cmd="run.pl --mem 4G"
-#export cuda_cmd="run.pl --mem 4G --gpu 1"
-#export decode_cmd="run.pl --mem 4G"
-
-# JHU setup (copy queue-freegpu.pl from ESPnet into utils/)
-export train_cmd="queue.pl --mem 4G"
 export cuda_cmd="queue.pl --mem 10G --gpu 1 --config conf/gpu.conf"
-export decode_cmd="queue.pl --mem 4G"
+export train_cmd="queue.pl --mem 8G --nodes_rack 4"
+export decode_cmd="queue.pl --mem 8G --nodes_rack 4"
+export train_cmd_intel="queue.pl --mem 8G --nodes_rack 4"
+export train_cmd_all_intel="queue.pl --mem 8G --remove_nodes_rack 7"
+export train_cmd_r5="queue.pl --mem 16G --nodes_rack 5 --gpu_queue 1"
+export train_cmd_r7="queue.pl --mem 16G --nodes_rack 7 --gpu_queue 1"
+export train_cmd_r6="queue.pl --mem 16G --nodes_rack 6 --gpu_queue 1"
+export train_cmd_r8="queue.pl --mem 16G --nodes_rack 8 --gpu_queue 1"
+export train_cmd_tesla="queue.pl --mem 16G --nodes_tesla 1 --gpu_queue 1"
+export gpu_cmd="queue.pl --mem 8G --gpu 1"
+
+
+
+
--- egs/safet/asr/simple_v1/path.sh
+++ egs/safet/asr/simple_v1/path.sh
@@ -1,3 +1,8 @@
-export SNOWFALL_ROOT=`pwd`/../../../..
-[ -f $SNOWFALL_ROOT/tools/env.sh ] && . $SNOWFALL_ROOT/tools/env.sh
+export KALDI_ROOT=`pwd`/../../..
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+. $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
+. /etc/profile.d/modules.sh
+module load shared cuda80/toolkit
