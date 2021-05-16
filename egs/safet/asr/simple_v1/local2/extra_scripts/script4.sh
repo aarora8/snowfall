9799a77fb1be174b576200eaef7e0cce03e57124
aarora8 <ashish.arora.88888@gmail.com>
2021-05-15 20:09:41 -0400

    minor change

--- egs/safet/asr/simple_v1/local2/extra_scripts/script4.sh
+++ egs/safet/asr/simple_v1/local2/extra_scripts/script4.sh
@@ -8,7 +8,11 @@
 #$ -M ashish.arora.88888@gmail.com
 #$ -m bea
 #$ -l mem_free=16G
-#$ -l h_rt=1:00:00
+#$ -l h_rt=24:00:00
+#$ -o decode.log
+
+# big data config
+# qsub -l gpu=4 -q gpu.q@@v100 -l h_rt=72:00:00
 
 # Activate dev environments and call programs
 source ~/.bashrc
@@ -21,5 +25,8 @@ module load cuda10.0/toolkit
 env | grep CUDA_VISIBLE_DEVICES
 nvidia-smi
 
+echo "$0: Running the job on GPU(s) $CUDA_VISIBLE_DEVICES"
+"$@"
+
 #/home/hltcoe/aarora/miniconda3/envs/k2/bin/python3 mmi_bigram_decode.py
-bash run.sh
+#bash run.sh
