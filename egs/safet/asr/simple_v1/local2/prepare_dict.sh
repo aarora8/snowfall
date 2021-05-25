650f58a3ad1a61b01acde3928a6e32b73f34e335
aarora8 <ashish.arora.88888@gmail.com>
2021-05-25 11:10:33 -0400

    minor fix

--- egs/safet/asr/simple_v1/local2/prepare_dict.sh
+++ egs/safet/asr/simple_v1/local2/prepare_dict.sh
@@ -4,11 +4,12 @@ dst_dir=data/local/dict_nosp
 silence_phones=$dst_dir/silence_phones.txt
 optional_silence=$dst_dir/optional_silence.txt
 nonsil_phones=$dst_dir/nonsilence_phones.txt
-lexicon_raw_nosil=$dst_dir/lexicon/librispeech-lexicon.txt
+lexicon_raw_nosil=$dst_dir/lexicon/lexicon_raw_nosil.txt
 
 mkdir -p $dst_dir/lexicon
 wget -P $dst_dir/lexicon/ https://www.openslr.org/resources/11/librispeech-lexicon.txt
-cp $dst_dir/lexicon/librispeech-lexicon.txt  $dst_dir/lexicon/lexicon_raw_nosil.txt
+cat $dst_dir/lexicon/librispeech-lexicon.txt  | \
+  perl -ne '($a, $b) = split " ", $_, 2; $b =~ s/[0-9]//g; print "$a $b";' > $lexicon_raw_nosil
 
 echo "Preparing phone lists"
 (echo SIL; echo SPN;) > $silence_phones
