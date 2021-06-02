8cafe492ba045a79dcc1b582b057d592824230be
aarora8 <ashish.arora.88888@gmail.com>
2021-06-02 12:54:11 -0400

    minor fix

--- egs/safet/asr/simple_v1/mmi_att_transformer_train.py
+++ egs/safet/asr/simple_v1/mmi_att_transformer_train.py
@@ -564,12 +564,12 @@ def run(rank, world_size, args):
         ali_model_fname = Path(f'exp-lstm-adam-mmi-bigram-musan-dist-s4/epoch-{args.ali_model_epoch}.pt')
         assert ali_model_fname.is_file(), \
                 f'ali model filename {ali_model_fname} does not exist!'
-        ali_model.load_state_dict(torch.load(ali_model_fname, map_location='cpu')['state_dict'])
+        checkpoint = torch.load(ali_model_fname, map_location='cpu')
+        loaded_dict = checkpoint['state_dict']
+        loaded_dict = {key: loaded_dict[key] for key in ali_model.state_dict()}
+        ali_model.load_state_dict(loaded_dict)
+        #ali_model.load_state_dict(torch.load(ali_model_fname, map_location='cpu')['state_dict'])
         ali_model.to(device)
-        #checkpoint = torch.load(ali_model_fname, map_location='cpu')
-        #loaded_dict = checkpoint['state_dict']
-        #loaded_dict = {key: loaded_dict[key] for key in ali_model.state_dict()}
-        #ali_model.load_state_dict(loaded_dict)
         #new_state_dict = OrderedDict()
         #for k, v in loaded_dict.items():
         #    if k != 'P_scores':
