9898d6cd432a88be4df6b746e9fac619e8ab5874
aarora8 <ashish.arora.88888@gmail.com>
2021-06-05 16:22:29 -0400

    minor fix

--- egs/safet/asr/simple_v1/mmi_bigram_train.py
+++ egs/safet/asr/simple_v1/mmi_bigram_train.py
@@ -304,7 +304,7 @@ def main():
         logging.info('Using BucketingSampler.')
         train_sampler = BucketingSampler(
             cuts_train,
-            max_frames=40000,
+            max_frames=10000,
             shuffle=True,
             num_buckets=30
         )
@@ -312,7 +312,7 @@ def main():
         logging.info('Using regular sampler with cut concatenation.')
         train_sampler = SingleCutSampler(
             cuts_train,
-            max_frames=30000,
+            max_frames=10000,
             shuffle=True,
         )
     logging.info("About to create train dataloader")
@@ -331,7 +331,7 @@ def main():
     #       torch.distributed.all_reduce() tends to hang indefinitely inside
     #       NCCL after ~3000 steps. With the current approach, we can still report
     #       the loss on the full validation set.
-    valid_sampler = SingleCutSampler(cuts_dev, max_frames=90000, world_size=1, rank=0)
+    valid_sampler = SingleCutSampler(cuts_dev, max_frames=10000, world_size=1, rank=0)
     logging.info("About to create dev dataloader")
     valid_dl = torch.utils.data.DataLoader(
         validate,
