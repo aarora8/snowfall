a5f9da4e5b5d579c079ce4956f6f23c0a3b32aee
aarora8 <ashish.arora.88888@gmail.com>
2021-06-08 18:15:24 -0400

    cosmetic fix

--- egs/safet/asr/simple_v1/local/replace_biphone_with_monophone_from_lexicon.py
+++ egs/safet/asr/simple_v1/local/replace_biphone_with_monophone_from_lexicon.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python
+
 import os
+import argparse
+parser = argparse.ArgumentParser(description="""creates left bi-phone lexicon from monophone lexicon""")
+parser.add_argument('lexicon', type=str, help='File name of a file that contains the'
+                    'lexicon with monophones. Each line must be: <word> <phone1> <phone2> ...')
+parser.add_argument('nonsilence_biphones', type=str, help='Output file that contains'
+                    'non-silence left bi-phones')
+parser.add_argument('biphone_lexicon', type=str, help='Output file that'
+                    'contains left bi-phone lexicon. Each line must be: <word> <biphone1> <biphone2> ...')
 
 def main():
-
-
-    biphone_text_handle = open('/Users/ashisharora/Desktop/corpora/kaldi_data_safet/e2e/output/text_to_biphones.txt', 'r', encoding='utf8')
+    args = parser.parse_args()
+    text_handle = open(args.output_monobi_lexicon, 'w', encoding='utf8')
+    biphone_text_handle = open(args.bitext, 'r', encoding='utf8')
     biphones_text_data = biphone_text_handle.read().strip().split("\n")
     biphone_train_count = dict()
     for line in biphones_text_data:
-        biphones = line.split(" ")
+        biphones = line.split(" ")[1:]
         for biphone in biphones:
             biphone_train_count[biphone] = 1
 
-
-    biphone_handle = open('/Users/ashisharora/Desktop/corpora/kaldi_data_safet/e2e/output/bilexicon.txt', 'r', encoding='utf8')
+    biphone_handle = open(args.bilexicon, 'r', encoding='utf8')
     biphones_lexicon = biphone_handle.read().strip().split("\n")
     word2biphone = dict()
     for key_val in biphones_lexicon:
-        key_val = key_val.split(" ")
+        key_val = key_val.strip().split(" ")
         word = key_val[0]
         word2biphone[word] = list()
         for biphone in key_val[1:]:
             if biphone not in biphone_train_count:
-                print(key_val[0])
-                print(biphone)
-                phone = biphone.split('_')[1]
-                word2biphone[word].append(phone) 
+                monophone = biphone.split('_')[1]
+                word2biphone[word].append(monophone)
             else:
                word2biphone[word].append(biphone)
 
-
-    text_file = os.path.join('/Users/ashisharora/Desktop/corpora/kaldi_data_safet/e2e/output/filtered_biphone_lexicon.txt')
-    text_fh = open(text_file, 'w')
-    for key in word2biphone:
-        text_fh.write(key + " " + " ".join(word2biphone[key]) + '\n')    
-
+    for word in word2biphone:
+        text_handle.write(word + " " + " ".join(word2biphone[word]) + '\n')
 
 if __name__ == '__main__':
     main()
